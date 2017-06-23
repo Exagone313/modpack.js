c68c4fa9ec735ab25ab896aef55f7dcc1856b899
Exagone313 <exa@elou.world>
2017-06-23 22:46:14 +0200

    Use Object.prototype explicitly

--- underscore/private/is.js
+++ underscore/private/is.js
@@ -1,4 +1,4 @@
 // Add some isType methods: isArguments, isFunction, isString, isNumber, isDate, isRegExp, isError, isMap, isWeakMap, isSet, isWeakSet.
 module.exports = function(name, obj) {
-	return toString.call(obj) === '[object ' + name + ']';
+	return Object.prototype.toString.call(obj) === '[object ' + name + ']';
 };
